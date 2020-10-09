a963ed70be484102d120795e8cfc4bcf4b0417ae
moinur-najib <moinurnajib@gmail.com>
2020-10-09 11:50:09 +0600

    Two Sums

---
+++ Two-Sums/index.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    var diff = 0;
+
+    for (var i = 0; i < nums.length; i++) {
+        diff = Math.abs(nums[i] - target);
+
+        if (nums.includes(diff)) {
+            if ()
+        }
+    }
+}
+
+var nums = [3, 2, 4];
+var target = 6;
+
+console.log(twoSum(nums, target));
