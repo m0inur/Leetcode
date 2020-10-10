f82e99ab377decc9c449677a240f641ee3be7423
moinur-najib <moinurnajib@gmail.com>
2020-10-10 13:37:26 +0600

    Rotate Array

---
+++ Rotate-Array/index.js
@@ -0,0 +1,9 @@
+var rotate = function (nums, k) {
+    while (k > 0) {
+        nums.unshift(nums[nums.length - 1]);
+        nums.splice(nums.length - 1, 1);
+        k--;
+    }
+
+    return nums;
+};
