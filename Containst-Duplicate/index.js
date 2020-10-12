fbc60dadcf2d08f19665ae225df63223a32e67a1
moinur-najib <moinurnajib@gmail.com>
2020-10-12 19:59:10 +0600

    Contains Duplicate

---
+++ Containst-Duplicate/index.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function (nums) {
+    nums.sort((a, b) => a - b);
+
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+
+    return false;
+};
