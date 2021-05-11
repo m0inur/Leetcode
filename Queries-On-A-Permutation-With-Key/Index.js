92d36e35441a15af7d6a88654d8fadfd4b777492
Moinur Najib <moinurnajib@gmail.com>
2021-05-11 15:25:01 +0600

    Queries on a Permutation With Key

---
+++ Queries-On-A-Permutation-With-Key/Index.js
@@ -0,0 +1,22 @@
+﻿/**
+ * @param {number[]} queries
+ * @param {number} m
+ * @return {number[]}
+ */
+let processQueries = function(queries, m) {
+    let permutations = [];
+    let ans = [];
+    
+    for(let i = 1; i <= m ; i++) {
+       permutations.push(i); 
+    }
+    
+    for(let i = 0; i < queries.length; i++) {
+        let index = permutations.indexOf(queries[i]);
+        ans.push(index);
+        permutations.splice(index, 1);
+        permutations.unshift(queries[i]);
+    }
+    
+    return ans;
+};
