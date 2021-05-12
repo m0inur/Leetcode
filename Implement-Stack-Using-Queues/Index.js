ebdbfcec18c964ae750ca9271a2a932e058e7c6e
Moinur Najib <moinurnajib@gmail.com>
2021-05-12 16:13:07 +0600

    Implement Stack using Queues

---
+++ Implement-Stack-Using-Queues/Index.js
@@ -0,0 +1,39 @@
+﻿/**
+ * Initialize your data structure here.
+ */
+let MyStack = function() {
+    this.queue = [];
+};
+
+/**
+ * Push element x onto stack.
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.unshift(x);
+};
+
+/**
+ * Removes the element on top of the stack and returns that element.
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.queue.splice(0, 1);
+};
+
+/**
+ * Get the top element.
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[0];
+};
+
+/**
+ * Returns whether the stack is empty.
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0;
+};
