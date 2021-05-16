c4ec075ff8fc60900565addc819e51213089e2cb
Moinur Najib <moinurnajib@gmail.com>
2021-05-16 13:28:48 +0600

    Seat Reservation Manager

---
+++ Seat-Reservation-Manager/Index.js
@@ -0,0 +1,46 @@
+﻿/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+let permuteUnique = function(nums) {
+    let permutations = [];
+    let uniquePermutations = [];
+    let nonUniquePermutations = [];
+    p_helper(nums, 0, permutations);
+    
+    for(let i = 0; i < permutations.length; i++) {
+        let joinedUniquePermutations = [...uniquePermutations].join('');
+        if(!joinedUniquePermutations.includes(permutations[i])) {
+            uniquePermutations.push(permutations[i])
+        } else {
+            nonUniquePermutations.push(permutations[i].join(''));
+            console.log(nonUniquePermutations);
+        }
+    }
+    
+    return uniquePermutations;
+};
+
+function p_helper(nums, index, results) {
+    // If its the last index
+    if (index === nums.length) {
+        results.push([...nums]);
+    }
+
+    // Start from the given index
+    for (let i = index; i < nums.length; i++) {
+        // Swap the given index with i
+        swap(nums, index, i);
+        p_helper(nums, index+1, results);
+        swap(nums, index, i);
+    }
+}
+
+function swap(nums, i1, i2) {
+    let tmp = nums[i1];
+    nums[i1] = nums[i2];
+    nums[i2] = tmp;
+}
+
+let nums = [1,1,2];
+console.log(permuteUnique(nums));
