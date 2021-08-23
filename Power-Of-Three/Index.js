2ab0b0ba1ea0bc68c2027ee188da4fcfac4a31a6
Moinur Najib <moinurnajib@gmail.com>
2021-08-23 19:00:57 +0600

    Power-Of-Three

---
+++ Power-Of-Three/Index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+let isPowerOfThree = function(n) {
+    if(n === 0) return false;
+    while(n !== 1) {
+        if(n % 3 !== 0) {
+            return false
+        }
+        n = Math.floor(n/3)
+    }
+    return true
+};
